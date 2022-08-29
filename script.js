616d845d8795f550491acd38c94e9e46596642be
Raissa Barbosa <raissabbsa@gmail.com>
2022-08-29 09:20:07 -0300

    Versao 3 - com envio de mensagem funcionando parcialmente

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 
 let nome="";
+let nomeAtual;
 
 function entradaSala() {
     
     const nomeDigitado = document.querySelector('.telaInicial input')
+    nomeAtual = nomeDigitado.value;
 
     nome = {
-        name: nomeDigitado.value
+        name: nomeAtual
     }
-
-    console.log(nome);
 
     const requisicao = axios.post('https://mock-api.driven.com.br/api/v6/uol/participants', nome);
 
@@ -18,59 +18,114 @@ function entradaSala() {
 
 }
 
+function tratarErro() {
+    alert("Já existe um usuário com esse nome, digite outro nome:");
+}
+
+function tratarSucesso() {
+
+    const tela = document.querySelector('.telaInicial');
+    tela.classList.remove('visivel');
+
+    carregaMensagem();
+    setTimeout(avisapresenca, 5000);
+}
+setTimeout(carregaMensagem, 3000);
+
 function carregaMensagem() {
     const promessa = axios.get('https://mock-api.driven.com.br/api/v6/uol/messages');
     promessa.then(carregouMensagem);
 }
 
+
 function carregouMensagem (mensagem) {
 
     todasMensagens = mensagem.data;
-
-    console.log(todasMensagens);
 
     const pegaDiv = document.querySelector('main');
 
     for(let i=0; i<todasMensagens.length;i++) {
 
         if(todasMensagens[i].type == "message") {
-            pegaDiv.innerHTML += `<div class="enviaMensagem">
-            <div class="hora">(${todasMensagens[i].time})</div>
-            <div class="conteudo">${todasMensagens[i].from} para ${todasMensagens[i].to}: ${todasMensagens[i].text}</div>
-        </div>`
+            
+            if(i === todasMensagens.length-1) {
+                pegaDiv.innerHTML += `<div class="enviaMensagem ultimaMensagem">
+                <div class="hora">(${todasMensagens[i].time})</div>
+                <div class="conteudo">${todasMensagens[i].from} para ${todasMensagens[i].to}: ${todasMensagens[i].text}</div>
+            </div>`}
+
+            else { 
+                pegaDiv.innerHTML += `<div class="enviaMensagem">
+                <div class="hora">(${todasMensagens[i].time})</div>
+                <div class="conteudo">${todasMensagens[i].from} para ${todasMensagens[i].to}: ${todasMensagens[i].text}</div>
+            </div>`}
         }
 
         else if(todasMensagens[i].type == "status") {
-            pegaDiv.innerHTML+=`<div class="enviaStatus">
+            if(i === todasMensagens.length-1) {
+            pegaDiv.innerHTML+=`<div class="enviaStatus ultimaMensagem">
             <div class="hora">(${todasMensagens[i].time})</div>
             <div class="conteudo">${todasMensagens[i].from}: ${todasMensagens[i].text}</div>
-        </div>`
+        </div>`}
+
+            else {
+                pegaDiv.innerHTML+=`<div class="enviaStatus">
+                <div class="hora">(${todasMensagens[i].time})</div>
+                <div class="conteudo">${todasMensagens[i].from}: ${todasMensagens[i].text}</div>
+            </div>`
+            }
         }
 
-        else if (todasMensagens[i].type == "private_message") {
-            pegaDiv.innerHTML+=`<div class="enviaReservado">
-            <div class="hora">(${todasMensagens[i].time})</div>
-            <div class="conteudo">${todasMensagens[i].from} reservadamente para ${todasMensagens[i].to}: ${todasMensagens[i].text}</div>
-        </div>`
-        }
-        
-    }
+        else if (todasMensagens[i].type === "private_message") {
+            if(todasMensagens[i].to == nomeAtual) {
+                if(i === todasMensagens.length-1) {
+                    pegaDiv.innerHTML+=`<div class="enviaReservado ultimaMensagem">
+                    <div class="hora">(${todasMensagens[i].time})</div>
+                    <div class="conteudo">${todasMensagens[i].from} reservadamente para ${todasMensagens[i].to}: ${todasMensagens[i].text}</div>
+                </div>`
+                }
+                else {
+                    pegaDiv.innerHTML+=`<div class="enviaReservado">
+                    <div class="hora">(${todasMensagens[i].time})</div>
+                    <div class="conteudo">${todasMensagens[i].from} reservadamente para ${todasMensagens[i].to}: ${todasMensagens[i].text}</div>
+                </div>`}
+            }
+        }  
+    } 
+    divFinal =document.querySelector('.ultimaMensagem') ;
+    divFinal.scrollIntoView();
 }
 
-setTimeout(carregaMensagem, 3000);
+function enviaMensagem() {
 
-/*setTimeout(avisapresenca, 5000);
+    const pegaMensagem = document.querySelector('footer input');
+
+    let mensagem = {
+        from: nomeAtual,
+        to: "Todos",
+        text: pegaMensagem.value,
+        type: "message"
+    }
+
+    requisicao = axios.post('https://mock-api.driven.com.br/api/v6/uol/messages', mensagem);
+
+    requisicao.then(enviouMensagem);
+}
+
+function enviouMensagem() {
+    carregaMensagem();  
+}
+
 
 function avisapresenca() {
-    const requisicao = axios.post('https://mock-api.driven.com.br/api/v6/uol/status', nome);
-}*/
-
-function tratarErro() {
-        alert("Já existe um usuário com esse nome, digite outro nome:");
+    const pedido = axios.post('https://mock-api.driven.com.br/api/v6/uol/status', nome);
+    pedido.catch(ausente);
 }
 
-function tratarSucesso() {
+function ausente() {
+
     const tela = document.querySelector('.telaInicial');
-    tela.classList.remove('visivel');
-    carregaMensagem();
+    tela.classList.add('visivel');
+    entradaSala();
+
 }
